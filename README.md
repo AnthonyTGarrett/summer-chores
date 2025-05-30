47402b198dedac2c861186b3e854bbdb1f6c4c64
Anthony Garrett <tony.garrett@gmail.com>
2025-05-30 17:28:26 -0400

    fist commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Summer Chores
+
+## Script to simulate doing summer chores with a chance of getting tired and giving up
---
+++ callbackVersion.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function mowYard(name, callback) {
+  setTimeout(() => {
+    console.log(`${name} mowed the yard.`);
+  }, 2000);
+  weedEat(name, trimHedges);
+}
+function weedEat(name, callback) {
+  console.log(`${name} finished using the weed eater.`);
+  console.log(`${name} fell asleep after moving the yard.`);
+}
+function trimHedges(name, callback) {
+  console.log(`${name} finished trimming the hedges.`);
+  console.log(`${name} fell asleep after trimming the hedges.`);
+}
+function collectWood(name, callback) {
+  console.log(`${name} finished collecting wood.`);
+  console.log(`${name} fell asleep after collecting wood.`);
+}
+function waterGarden(name, callback) {
+  console.log(`${name} finished watering the garden.`);
+  console.log(`${name} fell asleep after watering the garden.`);
+}
+function doSummerChores(name) {
+  mowYard(name, weedEat);
+  console.log(`${name} finished all their chores.`);
+}
+
+doSummerChores('Tony');
