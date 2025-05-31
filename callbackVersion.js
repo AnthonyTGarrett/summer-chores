182454e377f40005c47640c34db5ff376e11c843
Tony Garrett <tony.garrett@gmail.com>
2025-05-30 21:16:04 -0400

    Need help

--- callbackVersion.js
+++ callbackVersion.js
@@ -4,26 +4,45 @@ function mowYard(name, callback) {
   setTimeout(() => {
     console.log(`${name} mowed the yard.`);
   }, 2000);
-  weedEat(name, trimHedges);
+  callback(name, callback);
 }
 function weedEat(name, callback) {
   setTimeout(() => {
     console.log(`${name} finished using the weed eater.`);
   }, 1500);
-
-  console.log(`${name} fell asleep after moving the yard.`);
+  if (Math.random() < 0.9) {
+    console.log(`${name} fell asleep after mowing the yard.`);
+    return;
+  }
+  callback(name, callback);
 }
 function trimHedges(name, callback) {
-  console.log(`${name} finished trimming the hedges.`);
-  console.log(`${name} fell asleep after trimming the hedges.`);
+  setTimeout(() => {
+    console.log(`${name} finished trimming the hedges.`);
+  }, 1000);
+  if (Math.random() < 0.4) {
+    console.log(`${name} fell asleep after weed eating the yard.`);
+    return;
+  }
+  callback(name, callback);
 }
 function collectWood(name, callback) {
-  console.log(`${name} finished collecting wood.`);
-  console.log(`${name} fell asleep after collecting wood.`);
+  setTimeout(() => {
+    console.log(`${name} finished collecting wood.`);
+  }, 2500);
+  if (Math.random() < 0.6) {
+    console.log(`${name} fell asleep after trimming the hedges.`);
+    return;
+  }
+  callback(name, callback);
 }
 function waterGarden(name, callback) {
-  console.log(`${name} finished watering the garden.`);
-  console.log(`${name} fell asleep after watering the garden.`);
+  setTimeout(() => {
+    console.log(`${name} finished watering the garden.`);
+  });
+  if (Math.random() < 0.8)
+    console.log(`${name} fell asleep after collecting the wood.`);
+  callback(name, callback);
 }
 function doSummerChores(name) {
   mowYard(name, weedEat);
